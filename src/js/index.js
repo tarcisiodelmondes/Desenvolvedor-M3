8d2e442de5408f2e3266f108505ab65e90be7207
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-11-13 20:38:05 -0300

    feat: adding order menu

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,34 @@
 import { cart } from "./components/cart";
+import { toggleOrderMenu } from "./functions/toggleOrderMenu";
+import { clearClasses } from "./utils/clearClasses";
 
 const productsInCart = [];
 
 const headerCart = document.querySelector(".header__cart");
-
 headerCart.appendChild(cart(productsInCart));
+
+const selectMenuOrderBtn = document.querySelector(".select__btn");
+selectMenuOrderBtn.addEventListener("click", () => {
+  toggleOrderMenu();
+});
+
+const selectListMenu = document.querySelectorAll(".select__list li");
+
+selectListMenu.forEach((menuItem, index) => {
+  menuItem.addEventListener("click", () => {
+    menuItem.classList.toggle("active");
+
+    selectListMenu.forEach((item, itemIndex) => {
+      if (itemIndex === index) {
+        return;
+      }
+      const isItemActive = item.classList.contains("active");
+
+      if (isItemActive) {
+        clearClasses([item], ["active"]);
+      }
+    });
+
+    toggleOrderMenu();
+  });
+});
